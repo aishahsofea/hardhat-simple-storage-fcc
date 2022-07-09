87f8d46ba696284cc269c771c76ca2058bd26bd8
Aishah Sofea <aishah.s.hanifa@gmail.com>
2022-07-09 09:16:39 +0800

    interacts with smart contracts using hardhat

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,20 +3,25 @@ require("dotenv").config();
 
 const RINKEBY_RPC_URL = process.env.RINKEBY_RPC_URL;
 const PRIVATE_KEY = process.env.PRIVATE_KEY;
-const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "";
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   defaultNetwork: "hardhat",
   networks: {
+    hardhat: {},
     rinkeby: {
       url: RINKEBY_RPC_URL,
       accounts: [PRIVATE_KEY],
       chainId: 4,
     },
-    etherscan: {
-      apiKey: ETHERSCAN_API_KEY,
+    localhost: {
+      url: "http://localhost:8545",
+      chainId: 31337,
     },
   },
   solidity: "0.8.8",
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY,
+  },
 };
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -11,6 +11,14 @@ async function main() {
     await simpleStorage.deployTransaction.wait(6);
     await verify(simpleStorage.address, []);
   }
+
+  const currentValue = await simpleStorage.retrieve();
+  console.log(`Current value is: ${currentValue}`);
+
+  const transactionResponse = await simpleStorage.store(7);
+  await transactionResponse.wait(1);
+  const updatedValue = await simpleStorage.retrieve();
+  console.log(`Updated value is: ${updatedValue}`);
 }
 
 async function verify(contractAddress, args) {
