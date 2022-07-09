4fa989b539b5175f22332d183e4b124a3de13717
Aishah Sofea <aishah.s.hanifa@gmail.com>
2022-07-09 09:03:16 +0800

    programatic verification

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,12 +1,32 @@
-const { ethers } = require("hardhat");
+const { ethers, run, network } = require("hardhat");
 
 async function main() {
   const SimpleStorageFactory = await ethers.getContractFactory("SimpleStorage");
   console.log("Deploying contract...");
   const simpleStorage = await SimpleStorageFactory.deploy();
   await simpleStorage.deployed();
-
   console.log(`Contract deployed to ${simpleStorage.address}`);
+
+  if (network.config.chainId === 4 && process.env.ETHERSCAN_API_KEY) {
+    await simpleStorage.deployTransaction.wait(6);
+    await verify(simpleStorage.address, []);
+  }
+}
+
+async function verify(contractAddress, args) {
+  console.log("Verifying contract...");
+  try {
+    await run("verify:verify", {
+      address: contractAddress,
+      constructorArguments: args,
+    });
+  } catch (e) {
+    if (e.message.toLowerCase().includes("already verified")) {
+      console.log("Already Verified!");
+    } else {
+      console.log(e);
+    }
+  }
 }
 
 main()
