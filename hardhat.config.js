88e16838be93d80fd52b0640a9ee00c9d0e971d1
Aishah Sofea <aishah.s.hanifa@gmail.com>
2022-07-12 21:43:25 +0800

    add solidity coverage

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,11 +2,12 @@ require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config();
 require("./tasks/block-number");
 require("hardhat-gas-reporter");
+require("solidity-coverage");
 
 const RINKEBY_RPC_URL = process.env.RINKEBY_RPC_URL || "";
 const PRIVATE_KEY = process.env.PRIVATE_KEY || "";
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "";
-const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY || "";
+const COINMARKETCAP_API_KEY = proscess.env.COINMARKETCAP_API_KEY || "";
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
